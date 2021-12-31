586f7a532c326259f06ca176f5ca4f11c0b87300
Ayush85 <ayushrestha8585@gmail.com>
2021-12-31 20:22:53 +0545

    Updates

---
+++ precache-manifest.2ead229bc1cb4e9e9f3b598436084fd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58340be630090319a8b374d66de5a276",
+    "url": "/disease-prediction/index.html"
+  },
+  {
+    "revision": "e7eb7a061115382ff6a3",
+    "url": "/disease-prediction/static/css/main.bd13b50d.chunk.css"
+  },
+  {
+    "revision": "ad3011d31ddfa19666e4",
+    "url": "/disease-prediction/static/js/2.a2dde473.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/disease-prediction/static/js/2.a2dde473.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7eb7a061115382ff6a3",
+    "url": "/disease-prediction/static/js/main.0e8780c6.chunk.js"
+  },
+  {
+    "revision": "7bf649bb91dfa3824881",
+    "url": "/disease-prediction/static/js/runtime-main.b4d20f41.js"
+  }
+]);
